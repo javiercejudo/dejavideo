b82b23278849f38e5879891e328cc588181e7412
Javier Cejudo <javier@javiercejudo.com>
2012-11-05 00:20:33 +0100

    new classes and small details

--- display.php
+++ display.php
@@ -1,19 +1,19 @@
 <?php 
 if ($video) {
-	echo "<video class='display' controls='controls' autoplay='autoplay' poster='img/dejavideo.png'>";
+	echo "<video class='display display_video' controls='controls' autoplay='autoplay' poster='img/dejavideo.png'>";
 	echo "<source src='" . $video . "' type='" . $v_type . ";'>";
 	echo "Your browser does not support the video tag.";
 	echo "</video>";
-	echo "<p>";
+	echo "<p class='current_container'>";
 	echo "Current video: ";
 	echo "<span class='current' title='" . $n_video . "'>" . basename($n_video) . "</span>";
 	echo " <a href='$video'>[download]</a>";
-	echo " <a href='?v=" . $dir . "'><strong>[close]</strong></a>";
+	echo " <a href='?v=" . $dir . "#listing'><strong>[close]</strong></a>";
 	echo "</p>";
 } else {
-	echo "<h1 class='title'>DejaVideo by<br>@javiercejudo</h1>";
+	echo "<h1 class='title'><a href='.#listing'>DejaVideo</a> by <a href='https://twitter.com/javiercejudo'>@javiercejudo</a></h1>";
 	echo "<div class='display display_novideo'></div>";
-	echo "<p>";
+	echo "<p class='current_container'>";
 	echo "Current location: ";
 	echo "<span class='current' title='" . $dir . "'>" . $dir . "</span>";
 	if ($dir !== DATA ) {
