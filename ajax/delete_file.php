f2fd41f87af1946396e5b63ff139ba2699335da9
Javier Cejudo <javier@javiercejudo.com>
2013-05-25 21:11:24 +1000

    simplifies logic for deletion

--- ajax/delete_file.php
+++ ajax/delete_file.php
@@ -4,12 +4,8 @@ require '../functions.php';
 
 $path = $_POST['path'];
 
-if (substr($path, 0, strlen(DATA)) == DATA) {
-	if (delete_file($path)) {
-		echo 1;
-	} else {
-		echo 0;
-	}
-} else {
-	echo 0;
+if (substr($path, 0, strlen(DATA)) == DATA && delete_file($path)) {
+	echo 1;
+	exit;
 }
+echo 0;
